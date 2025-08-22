783f583b89a91a6b8c0cd8bf37c026c90d85b896
Lydia Haile <l.haile@imperial.ac.uk>
2025-08-22 22:25:39 +0100

    debug site file function for countries with vivax and falciparum

--- R/site_file.R
+++ R/site_file.R
@@ -5,7 +5,7 @@
 #' @export
 remove_zero_eirs<- function(iso3c, sites){
 
-  if(nrow(sites$eir) != nrow(sites$sites)){
+
 
     full_sites<- sites$sites |>
       mutate(site_ur := paste0(name_1, '_', urban_rural))
@@ -13,10 +13,12 @@ remove_zero_eirs<- function(iso3c, sites){
 
 
     full_eirs<- sites$eir |>
+      filter(sp== 'pf') |>
       mutate(site_ur := paste0(name_1, '_', urban_rural))
 
 
-    no_eir<- setdiff(unique(full_sites$site_ur), unique(full_eirs$site_ur))
+    if(nrow(full_sites) != nrow(full_eirs)){
+    no_eir<- setdiff(unique(full_eirs$site_ur), unique(full_sites$site_ur))
 
     remove<- full_sites |>
       filter(site_ur %in% no_eir) |>
